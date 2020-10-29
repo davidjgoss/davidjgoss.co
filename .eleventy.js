f623e846d44b7dadaaa17b0da5e11406f2de1da2
David Goss <dgoss@whiteclarkegroup.com>
2020-10-29 11:06:40 +0000

    fix dates, clean up article templating

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,16 @@
+const {DateTime} = require("luxon");
+
 module.exports = eleventyConfig => {
-    eleventyConfig.addPassthroughCopy('css');
-    eleventyConfig.addPassthroughCopy('fonts');
-    eleventyConfig.addPassthroughCopy('js');
-    eleventyConfig.addCollection('posts', collectionApi => {
-        return collectionApi.getFilteredByGlob('blog/*.md').reverse();
+    eleventyConfig.addFilter("humanDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: "utc"}).toLocaleString(DateTime.DATE_FULL);
+    });
+    eleventyConfig.addFilter("machineDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: "utc"}).toFormat("yyyy-LL-dd");
+    });
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("fonts");
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addCollection("posts", collectionApi => {
+        return collectionApi.getFilteredByGlob("blog/*.md").reverse();
     });
 };
