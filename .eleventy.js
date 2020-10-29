64c236d807bc5b3944f7b7773881eb44c74b81ee
David Goss <dgoss@whiteclarkegroup.com>
2020-10-29 09:43:41 +0000

    wip start to get css passing through

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = eleventyConfig => {
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('fonts');
+    eleventyConfig.addPassthroughCopy('js');
     eleventyConfig.addCollection('posts', collectionApi => {
         return collectionApi.getFilteredByGlob('blog/*.md').reverse();
     });
