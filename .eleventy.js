15495b9ca34e03cfb7c120f404fdfb7adb8c6f01
David Goss <dgoss@whiteclarkegroup.com>
2020-10-29 12:32:35 +0000

    rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const {DateTime} = require("luxon");
 const currentYear = new Date().getFullYear().toString();
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = eleventyConfig => {
     eleventyConfig.addFilter("humanDate", dateObj => {
@@ -11,6 +12,7 @@ module.exports = eleventyConfig => {
     });
     eleventyConfig.addShortcode("currentYear", () => currentYear);
     eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPlugin(pluginRss);
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addCollection("posts", collectionApi => {
         return collectionApi.getFilteredByGlob("blog/*.md").reverse();
